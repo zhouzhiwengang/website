2125b7c10a6f5f77ebb8fe51fb73e72bc4f95a10
Zxyzzg <zhouzhiwengang@163.com>
2019-04-20 23:49:52 +0800

    init

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from blog.models import *  # 导入所有模型类
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'pub_time')  # 文章列表的显示项
+
+
+admin.site.register((Category, Comment, Tag))  # 多个模块注册到后台
